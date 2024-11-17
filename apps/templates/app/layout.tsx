d7b549ab66e12dd7d63bebeddcca7c208018d4c4
Omar Shehab <531omarhero@gmail.com>
2024-11-17 14:00:04 +0200

    Add Doom UI Pro

---
+++ apps/templates/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+import "@styles/globals.css";
+import Provider from "@app/provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Doom UI Pro",
+  description: "Doom UI Pro Templates to make web apps in minutes.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
+}
